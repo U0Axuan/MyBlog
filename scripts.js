879b1439bc1dfb15aef3782c048ebc964893f18a
unknown <>
2016-04-15 21:02:22 +0800

    https://github.com/U0Axuan/MyBlog.git

---
+++ scripts.js
@@ -0,0 +1,8 @@
+//JavaScript Document
+document.write("hello");
+
+//创建日期对象 now，并实现当前日期输出
+{
+	var now=new Date();
+	document.write("<h2>今天日期："+now.getYear()+"年"+(now.getMonth( )+1+"月+now.getDate()+"日</h2>");
+}
